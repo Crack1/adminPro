f62303497e657e8b016e78a40ea76f5f48798172
Erwin Vides <erwinvides@hotmail.com>
2018-08-26 22:42:13 -0600

    First Commit

---
+++ src/app/shared/breadscrums/breadscrums.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadscrums',
+  templateUrl: './breadscrums.component.html',
+  styles: []
+})
+export class BreadscrumsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
