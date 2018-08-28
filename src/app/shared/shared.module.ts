a85bea9b33f975b5a3ba18eb6d347532fa0e504e
Erwin Vides <erwinvides@hotmail.com>
2018-08-27 21:05:28 -0600

    Modificaciones en los modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadscrumsComponent } from './breadscrums/breadscrums.component';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadscrumsComponent,
+    NopagefoundComponent,
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadscrumsComponent,
+    NopagefoundComponent,
+  ]
+})
+
+export class ShareModule { }
